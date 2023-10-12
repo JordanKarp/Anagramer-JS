80113c33db03606f60cdf54a06042b69daad260c
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-10-12 11:46:59 -0700

    Update GuessStatsBoard.jsx

--- src/components/GuessStatsBoard.jsx
+++ src/components/GuessStatsBoard.jsx
@@ -1,4 +1,9 @@
+import formatNumber from "../utils/formatNumber"
+
 const GuessStatsBoard = ({guessStats}) => {
+    let strictAcc = formatNumber(guessStats.correct / guessStats.total)
+    let acc = formatNumber(guessStats.correct / (guessStats.total - guessStats.repeat - guessStats.freebies))
+
     return (
         <div className='guessStatsBoard'>
             <p>Total Guesses: {guessStats.total}</p>
@@ -6,6 +11,7 @@ const GuessStatsBoard = ({guessStats}) => {
             <p>Incorrect: {guessStats.incorrect}</p>
             <p>Repeat: {guessStats.repeat}</p>
             <p>Freebie: {guessStats.freebies}</p>
+            <p>Accuracy: {acc} ({strictAcc})</p>
         </div>
     )
 }
