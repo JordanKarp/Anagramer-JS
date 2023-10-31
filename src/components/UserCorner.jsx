3b23b78ad12486adb8f73c299fcb020b0ba99c83
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-10-31 08:58:52 -0700

    User Page

--- src/components/UserCorner.jsx
+++ src/components/UserCorner.jsx
@@ -7,23 +7,12 @@ const UserCorner = ({user}) => {
     if (user === undefined) {
       return
     } 
-    const sizes = {}
-    const vocabArray = [...vocab];
-    vocabArray.forEach((word) => {
-        if (sizes[word.length]){
-            sizes[word.length] += 1;
-        } else {
-            sizes[word.length] = 1;
-        }
-    })
 
     return (
         <div className='userCorner'>
             <p><b>{userName}</b></p>
             <p>Points: {formatNumber(points)} </p>
             <p>Freebies: {freebies} </p>
-            <p>2: {sizes[2]}</p>
-            <p>2: {sizes[3]}</p>
             <GuessStatsBoard guessStats={guessStats} />
         </div>
     )
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,12 +1,22 @@
 // import { useState } from 'react'
+import { useContext } from 'react'
 import '../App.css'
+import GameContext from '../context/Context'
+
 
 function UserPage() {
-  // const [count, setCount] = useState(0)
-
+  const [user, setUser] = useContext(GameContext)
+  
+  if (user === undefined) {
+    return
+  } 
   return (
     <>
-      <h1>UserPage</h1>
+      <h1>User Info</h1>
+      <h2>${user.username}</h2>
+      <h3>${user.points}</h3>
+      <h3>${user.freebies}</h3>
+      <GuessStatsBoard guessStats={user.guessStats} />
     </>
      )
 }
