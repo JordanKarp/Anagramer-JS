4b50e5ec7c79e99568facc6dc81279e89ceab3fa
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-10-31 11:30:36 -0700

    Update UserPage.jsx

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -31,7 +31,7 @@ function UserPage() {
       </ul>
       <hr/>
       <GuessStatsBoard guessStats={user.guessStats} />
-      <button onClick={() => javascript:history.back()}>Go Back</button>
+      <a href="javascript:history.back()">Go Back</a>
 
     </>
      )
