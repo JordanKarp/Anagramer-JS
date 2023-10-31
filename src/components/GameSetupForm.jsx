c2b60a42bf891a30d84d92fb7756de5b50fff7c0
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-10-31 12:30:33 -0700

    Update look

--- src/components/GameSetupForm.jsx
+++ src/components/GameSetupForm.jsx
@@ -11,12 +11,12 @@ const GameSetupForm = ({user, setSize, setWinType, setTimeType}) => {
     return (
         <form>
             <label>
-                Target Word Size:
+                Target Word Size: 
                 <input type="number" pattern="[0-9]*" step='1' name="size" min="3" max="7" onChange={e => setSize(e.target.value)} defaultValue={user.roundSize} />
             </label>
             <br/>
             <label>
-                Time Setting:
+                Time Setting: 
                 <select name='timeType' id="time" onChange={e => setTimeType(e.target.value)} defaultValue={user.roundTime}>
                     <option value="Slow">Slow</option>
                     <option value="Normal">Normal</option>
@@ -25,19 +25,12 @@ const GameSetupForm = ({user, setSize, setWinType, setTimeType}) => {
             </label>
             <br/>
             <label>
-                Win Type:
+                Win Type: 
                 <select name='winType' id="win" onChange={e => setWinType(e.target.value)} defaultValue={user.roundWin}>
                     <option value="60">Easy</option>
                     <option value="80">Medium</option>
                     <option value="100">Hard</option>
                 </select>
-
-                {/* <input type="radio" name='winType' value="60" onChange={e => setWinType(e.target.value)} defaultChecked={user.roundWin === '60'} />
-                <label htmlFor="60">Easy: 60%</label>
-                <input type="radio" name='winType' value="80" onChange={e => setWinType(e.target.value)} defaultChecked={user.roundWin === '80'} />
-                <label htmlFor="80">80%</label>
-                <input type="radio" name='winType' value="100" onChange={e => setWinType(e.target.value)} defaultChecked={user.roundWin === '100'} />
-                <label htmlFor="100">100%</label> */}
             </label>
         </form>
     )
--- src/components/GuessStatsBoard.jsx
+++ src/components/GuessStatsBoard.jsx
@@ -6,12 +6,39 @@ const GuessStatsBoard = ({guessStats}) => {
 
     return (
         <div className='guessStatsBoard'>
-            <p>Total Guesses: {guessStats.total}</p>
+            <h3>Guesses: </h3>
+            <table>
+                <tr>
+                    <td>Total:</td>
+                    <td>{guessStats.total}</td>
+                </tr>
+                <tr>
+                    <td>Correct:</td>
+                    <td>{guessStats.correct}</td>
+                </tr>
+                <tr>
+                    <td>Incorrect:</td>
+                    <td>{guessStats.incorrect}</td>
+                </tr>
+                <tr>
+                    <td>Repeat:</td>
+                    <td>{guessStats.repeat}</td>
+                </tr>
+                <tr>
+                    <td>Freebies:</td>
+                    <td>{guessStats.freebies}</td>
+                </tr>
+                <tr>
+                    <td>Accuracy:</td>
+                    <td>{acc}% ({strictAcc}%)</td>
+                </tr>
+            </table>
+            {/* <p>Total: {guessStats.total}</p>
             <p>Correct: {guessStats.correct}</p>
             <p>Incorrect: {guessStats.incorrect}</p>
             <p>Repeat: {guessStats.repeat}</p>
             <p>Freebie: {guessStats.freebies}</p>
-            <p>Accuracy: {acc}% ({strictAcc}%)</p>
+            <p>Accuracy: {acc}% ({strictAcc}%)</p> */}
         </div>
     )
 }
