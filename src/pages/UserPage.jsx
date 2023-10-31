59b55efd99d51869cf62d5c5bc2ddae972eff2e9
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-10-31 15:26:08 -0700

    Update UserPage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -3,6 +3,7 @@ import '../App.css'
 import GameContext from '../context/Context'
 import GuessStatsBoard from '../components/GuessStatsBoard'
 import data from "../../data/wordListDict-short.json"
+import formatNumber from '../utils/formatNumber'
 
 
 
@@ -33,15 +34,17 @@ function UserPage() {
     <>
       <h1>User Info</h1>
       <h2>{user.userName}</h2>
-      <h3>Points: {user.points}</h3>
+      <h3>Points: {formatNumber(user.points)}</h3>
       <h3>Freebies: {user.freebies}</h3>
       <hr/>
       <h3>Words found:</h3>
       <ul>
-        <li>2 Letter Words: {(vocabNumbers[2] / dictNumbers[2]) || 0}% ({vocabNumbers[2] || 0} found)</li>
-        <li>3 Letter Words: __% ({vocabNumbers[3] || 0} found)</li>
-        <li>4 Letter Words: __% ({vocabNumbers[4] || 0} found)</li>
-        <li>5 Letter Words: __% ({vocabNumbers[5] || 0} found)</li>
+        <li>2 Letter Words: {formatNumber(vocabNumbers[2] / dictNumbers[2]) || 0}% ({vocabNumbers[2] || 0} found)</li>
+        <li>3 Letter Words: {formatNumber(vocabNumbers[3] / dictNumbers[3]) || 0}% ({vocabNumbers[3] || 0} found)</li>
+        <li>4 Letter Words: {formatNumber(vocabNumbers[4] / dictNumbers[4]) || 0}% ({vocabNumbers[4] || 0} found)</li>
+        <li>5 Letter Words: {formatNumber(vocabNumbers[5] / dictNumbers[5]) || 0}% ({vocabNumbers[5] || 0} found)</li>
+        <li>6 Letter Words: {formatNumber(vocabNumbers[6] / dictNumbers[6]) || 0}% ({vocabNumbers[6] || 0} found)</li>
+        <li>7 Letter Words: {formatNumber(vocabNumbers[7] / dictNumbers[7]) || 0}% ({vocabNumbers[7] || 0} found)</li>
       </ul>
       <hr/>
       <GuessStatsBoard guessStats={user.guessStats} />
