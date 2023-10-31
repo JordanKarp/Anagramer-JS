772ee2de909bc597b273de997b3dab4d6c81cf4f
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-10-31 11:25:20 -0700

    Create GameOptions.jsx

---
+++ src/components/GameOptions.jsx
@@ -0,0 +1,56 @@
+import { calcMult, calcTotalMult } from "../data/mutlipliers"
+import { formatTime } from "../data/time"
+import formatNumber from "../utils/formatNumber"
+
+
+const GameOptions = ({user}) => {
+    
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <th></th>
+                    <th>Value</th>
+                    <th>Multiplier</th>
+                </tr>
+                <tr>
+                    <td><b>Size</b></td>
+                    <td><input type="number" pattern="[0-9]*" step='1' name="size" min="3" max="7" onChange={e => setSize(e.target.value)} defaultValue={user.roundSize} />}</td>
+                    <td>{formatNumber(calcMult('size', size))} x</td>
+                </tr>
+                <tr>
+                    <td><b>Time</b></td>
+                    <td>{timeType}</td>
+                    <td>{formatNumber(calcMult('time', timeType))} x</td>
+                </tr>
+                <tr>
+                    <td><b>Win</b></td>
+                    <td>{winType}%</td>
+                    <td>{formatNumber(calcMult('win', winType))} x</td>
+                </tr>
+                <tr>
+                    <td colSpan='3'>
+                        <hr size='1px' color='black'/>
+                    </td>
+                </tr>
+                <tr>
+                    <td><b>Total Multiplier</b></td>
+                    <td></td>
+                    <td>{formatNumber(calcTotalMult(size,timeType,winType))} x</td>
+                </tr>
+                <tr>
+                    <td><b>Total Time</b></td>
+                    <td></td>
+                    <td>{formatTime(time)}</td>
+                </tr>
+                <tr>
+                    <td><b>Freebies Avalable</b></td>
+                    <td></td>
+                    <td>{freebies}</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default GameSetupTable
