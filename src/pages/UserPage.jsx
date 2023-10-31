8ae9f0a2e0b5246478bd5610b5de6a421c641a4a
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-10-31 10:44:26 -0700

    Update UserPage.jsx

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react'
 import { useContext, useEffect } from 'react'
 import '../App.css'
 import GameContext from '../context/Context'
@@ -11,14 +10,29 @@ function UserPage() {
   if (user === undefined) {
     return
   } 
+
+  useEffect(() => {
+
+  }, [data])
   
   return (
     <>
       <h1>User Info</h1>
-      <h2>{user.username}</h2>
+      <h2>{user.userName}</h2>
       <h3>Points: {user.points}</h3>
       <h3>Freebies: {user.freebies}</h3>
+      <hr/>
+      <h3>Words found:</h3>
+      <ul>
+        <li>2 Letter Words: __% (XXX found)</li>
+        <li>3 Letter Words: __% (XXX found)</li>
+        <li>4 Letter Words: __% (XXX found)</li>
+        <li>5 Letter Words: __% (XXX found)</li>
+      </ul>
+      <hr/>
       <GuessStatsBoard guessStats={user.guessStats} />
+      <a href="javascript:history.back()">Go Back</a>
+
     </>
      )
 }
