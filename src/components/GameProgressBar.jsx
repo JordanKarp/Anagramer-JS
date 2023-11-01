6ff2ed70eea6e6c12570fe99c4e0f2609c2d57a0
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-10-31 21:52:58 -0700

    Update GameProgressBar.jsx

--- src/components/GameProgressBar.jsx
+++ src/components/GameProgressBar.jsx
@@ -1,12 +1,18 @@
-const GameProgressBar = ({value, max}) => {
+const GameProgressBar = ({value, max, winPercent}) => {
 
     const percent = value / max
     return (
-      <progress value={value} max={max}>
+      // <progress value={value} max={max}>
+      //   <div className="progressBar">
+      //       <span>Progress: {percent}%</span>
+      //   </div>
+      // </progress>
+
+      <meter value={value} min="0" max={max} high={max}>
         <div className="progressBar">
-            <span>Progress: {percent}%</span>
+            <span>{percent}%</span>
         </div>
-      </progress>
+      </meter>
 
     )
 }
