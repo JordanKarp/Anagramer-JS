a80c9208e624edbbff3aa21d332133d482557556
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-11-01 13:19:59 -0700

    Update UserWordsFound.jsx

--- src/components/UserWordsFound.jsx
+++ src/components/UserWordsFound.jsx
@@ -17,39 +17,42 @@ const UserWordsFoundData = ({vocabNumbers, dictNumbers, totalFound}) => {
               <th>Totals</th>
             </tr>
             <tr>
-              <td className='colorBox' style={{background:red}}></td>
+              <td className='colorBox' style={{background:'red'}}></td>
               <td>2</td>
               <td>{formatPercent(vocabNumbers[2] / dictNumbers[2])}%</td>
               <td><progress value={vocabNumbers[2] || 0} max={dictNumbers[2]}></progress></td>
               <td>{vocabNumbers[2] || 0} / {dictNumbers[2]}</td>
             </tr>
             <tr>
-              <td className='colorBox' style={{background:orange}}></td>
+              <td className='colorBox' style={{background:"orange"}}></td>
               <td>3</td>
               <td>{formatPercent(vocabNumbers[3] / dictNumbers[3])}%</td>
               <td><progress value={vocabNumbers[3] || 0} max={dictNumbers[3]}></progress></td>
               <td>{vocabNumbers[3] || 0} / {dictNumbers[3]}</td>
             </tr>
             <tr>
-              <td className='colorBox' style={{background:yellow}}></td>
+              <td className='colorBox' style={{background:'yellow'}}></td>
               <td>4</td>
               <td>{formatPercent(vocabNumbers[4] / dictNumbers[4])}%</td>
               <td><progress value={vocabNumbers[4] || 0} max={dictNumbers[4]}></progress></td>
               <td>{vocabNumbers[4] || 0} / {dictNumbers[4]}</td>
             </tr>
             <tr>
+              <td className='colorBox' style={{background:'green'}}></td>
               <td>5</td>
               <td>{formatPercent(vocabNumbers[5] / dictNumbers[5])}%</td>
               <td><progress value={vocabNumbers[5] || 0} max={dictNumbers[5]}></progress></td>
               <td>{vocabNumbers[5] || 0} / {dictNumbers[5]}</td>
             </tr>
             <tr>
+              <td className='colorBox' style={{background:'blue'}}></td>
               <td>6</td>
               <td>{formatPercent(vocabNumbers[6] / dictNumbers[6])}%</td>
               <td><progress value={vocabNumbers[6] || 0} max={dictNumbers[6]}></progress></td>
               <td>{vocabNumbers[6] || 0} / {dictNumbers[6]}</td>
             </tr>
             <tr>
+              <td className='colorBox' style={{background:'purple'}}></td>
               <td>7</td>
               <td>{formatPercent(vocabNumbers[7] / dictNumbers[7])}%</td>
               <td><progress value={vocabNumbers[7] || 0} max={dictNumbers[7]}></progress></td>
