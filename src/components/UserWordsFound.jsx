d1d02b7f40544cba30547f9cc588abd81578fa59
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-11-02 21:14:58 -0700

    Update UserWordsFound.jsx

--- src/components/UserWordsFound.jsx
+++ src/components/UserWordsFound.jsx
@@ -3,11 +3,6 @@ import formatPercent from "../utils/formatPercent"
 
 const UserWordsFoundData = ({vocabNumbers, dictNumbers, totalFound}) => {
 
-    const changeSizeNumColor = (event) => {
-      const r = document.querySelector(':root');
-      r.style.setProperty('--size-' + event.target.name + '-clr', event.target.value)
-    }
-
     return (
       <>
         <table>
@@ -20,42 +15,42 @@ const UserWordsFoundData = ({vocabNumbers, dictNumbers, totalFound}) => {
               <th>Totals</th>
             </tr>
             <tr>
-              <td><input type="color" name='2' defaultValue='#ff0000' onInput={changeSizeNumColor} disabled/></td>
+              <td><input type="color" defaultValue='#ff0000' disabled/></td>
               <td>2</td>
               <td>{formatPercent(vocabNumbers[2] / dictNumbers[2])}%</td>
               <td><progress value={vocabNumbers[2] || 0} max={dictNumbers[2]}></progress></td>
               <td>{vocabNumbers[2] || 0} / {dictNumbers[2]}</td>
             </tr>
             <tr>
-              <td><input type="color" name='3' defaultValue='#ffa500' onChange={changeSizeNumColor}/></td>
+              <td><input type="color" defaultValue='#ffa500' disabled/></td>
               <td>3</td>
               <td>{formatPercent(vocabNumbers[3] / dictNumbers[3])}%</td>
               <td><progress value={vocabNumbers[3] || 0} max={dictNumbers[3]}></progress></td>
               <td>{vocabNumbers[3] || 0} / {dictNumbers[3]}</td>
             </tr>
             <tr>
-              <td><input type="color" name='4' defaultValue='#ffff00' onChange={changeSizeNumColor}/></td>
+              <td><input type="color" defaultValue='#ffff00' disabled/></td>
               <td>4</td>
               <td>{formatPercent(vocabNumbers[4] / dictNumbers[4])}%</td>
               <td><progress value={vocabNumbers[4] || 0} max={dictNumbers[4]}></progress></td>
               <td>{vocabNumbers[4] || 0} / {dictNumbers[4]}</td>
             </tr>
             <tr>
-              <td><input type="color" name='5' defaultValue='#00ff00' onChange={changeSizeNumColor}/></td>
+              <td><input type="color" defaultValue='#00ff00' disabled/></td>
               <td>5</td>
               <td>{formatPercent(vocabNumbers[5] / dictNumbers[5])}%</td>
               <td><progress value={vocabNumbers[5] || 0} max={dictNumbers[5]}></progress></td>
               <td>{vocabNumbers[5] || 0} / {dictNumbers[5]}</td>
             </tr>
             <tr>
-              <td><input type="color" name='6' defaultValue='#0000ff' onChange={changeSizeNumColor}/></td>
+              <td><input type="color" defaultValue='#0000ff' disabled/></td>
               <td>6</td>
               <td>{formatPercent(vocabNumbers[6] / dictNumbers[6])}%</td>
               <td><progress value={vocabNumbers[6] || 0} max={dictNumbers[6]}></progress></td>
               <td>{vocabNumbers[6] || 0} / {dictNumbers[6]}</td>
             </tr>
             <tr>
-              <td><input type="color" name='4' defaultValue='#800080' onChange={changeSizeNumColor}/></td>
+              <td><input type="color" defaultValue='#800080' disabled/></td>
               <td>7</td>
               <td>{formatPercent(vocabNumbers[7] / dictNumbers[7])}%</td>
               <td><progress value={vocabNumbers[7] || 0} max={dictNumbers[7]}></progress></td>
