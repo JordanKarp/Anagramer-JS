7e2c9fb6152968cab33917c46e9950f830a0d2a1
Jordan Karp <Jordanmatthew.karp@gmail.com>
2023-11-02 21:06:50 -0700

    Update UserWordsFound.jsx

--- src/components/UserWordsFound.jsx
+++ src/components/UserWordsFound.jsx
@@ -3,17 +3,11 @@ import formatPercent from "../utils/formatPercent"
 
 const UserWordsFoundData = ({vocabNumbers, dictNumbers, totalFound}) => {
 
-    const loadDefaultNumColor = (event) => {
-      const r = document.querySelector(':root');
-      const picker = document.getElementById(event.target.id);
-      console.log(getComputedStyle(r).getPropertyValue('--size-' + event.target.name + '-clr'))
-      // picker.value = getComputedStyle(r).getPropertyValue('--size-' + event.target.name + '-clr')
-    }
     const changeSizeNumColor = (event) => {
       // const picker = document.getElementById(event.target.id);
       // picker.value = event.target.value
       const r = document.querySelector(':root');
-      console.log(getComputedStyle(r).getPropertyValue('--size-' + event.target.name + '-clr'))
+      // console.log(getComputedStyle(r).getPropertyValue('--size-' + event.target.name + '-clr'))
 
       r.style.setProperty('--size-' + event.target.name + '-clr', event.target.value)
     }
@@ -30,7 +24,7 @@ const UserWordsFoundData = ({vocabNumbers, dictNumbers, totalFound}) => {
               <th>Totals</th>
             </tr>
             <tr>
-              <td><input type="color" name='2' id='picker2' onInput={changeSizeNumColor} onLoad={loadDefaultNumColor}/></td>
+              <td><input type="color" name='2' defaultValue={'#ff0000'} onInput={changeSizeNumColor} /></td>
               <td>2</td>
               <td>{formatPercent(vocabNumbers[2] / dictNumbers[2])}%</td>
               <td><progress value={vocabNumbers[2] || 0} max={dictNumbers[2]}></progress></td>
