f2a700cde6e63150e372b26d08faf1d17624a1f0
Jordan Karp <jordanmatthew.karp@gmail.com>
2024-06-25 14:03:34 -0400

    Update Welcome.jsx

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -13,13 +13,17 @@ function Welcome() {
     e.preventDefault()
       // Send the login data to your server
 
-    if (e.nativeEvent.submitter.value === 'signIn') {
-      setMsg('signIn')
-    } else {
-      setMsg('register')
-    }
-    navigate('/Setup')
+    if (e.nativeEvent.submitter.value === 'play') {
+      setMsg('play')
+    navigate('/setup')
   }
+  //   if (e.nativeEvent.submitter.value === 'signIn') {
+  //     setMsg('signIn')
+  //   } else {
+  //     setMsg('register')
+  //   }
+  //   navigate('/Setup')
+  // }
 
   useEffect(() => {
     setUser({
@@ -31,8 +35,21 @@ function Welcome() {
   return (
     <>
       <h1 className="title">Anagramer</h1>
+      <h2 className="explaination">Create every word possible out of the given letters</h2>
+      <h3 className="explaination">Get 75% of all the words to proceed (100% for a bonus!)</h3>
+      <h3 className="explaination">Each word is 'learned' and will never need repeating - build your vocabulary!</h3>
       <div className="signIn">
         <form onSubmit={proceed} autoComplete="off">
+          <div>
+            <label htmlFor="userName">Name:</label>
+            <input type="text" name="userName" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='userName' />
+          </div>
+  
+           <div>
+            <button type='submit' name='play' value="play" formAction="/login">Play</button>
+           </div>
+        </form>
+        {/* <form onSubmit={proceed} autoComplete="off">
           <div>
             <label htmlFor="userName">User Name:</label>
             <input type="text" name="userName" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='userName' />
@@ -45,7 +62,7 @@ function Welcome() {
             <button type='submit' name='signIn' value="signIn" formAction="/login">Sign In</button>
             <button type='submit' name='register' value="register"formAction="/register">Register</button>
            </div>
-        </form>
+        </form> */}
         {msg && <p className="loginMsg">{msg}</p>}
       </div>
     </>
